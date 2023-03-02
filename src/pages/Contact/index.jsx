f4af8b0538a0fd469a48cd0aa4fe3b5ca8cdb736
Wenceslas Angelo <wenceslasangelo@gmail.com>
2023-03-02 10:36:53 +0300

    add map

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -30,37 +30,49 @@ function Contact() {
   return (
     <>
       <div className="contact-page">
-        <div className="text-zone">
-          <h1>Contact Me</h1>
-        </div>
+        <div className="contact-page-container">
+          <div className="text-zone">
+            <h1>Contact Me</h1>
+          </div>
 
-        <div className="contact-container">
-          <div className="image">
-            <img src={contact} alt="contact-image" />
+          <div className="contact-container">
+            <div className="image">
+              <img src={contact} alt="contact-image" />
+            </div>
+            <div className="contact-form">
+              <form ref={form} onSubmit={sendEmail}>
+                <ul>
+                  <li className="half">
+                    <input type="text" required placeholder="Name" />
+                  </li>
+                  <li className="half">
+                    <input type="email" required placeholder="Email" />
+                  </li>
+                  <li>
+                    <input type="text" required placeholder="Subject" />
+                  </li>
+                  <li>
+                    <textarea placeholder="Message" required></textarea>
+                  </li>
+                  <li>
+                    <button type="submit" className="flat-btn">
+                      SEND
+                    </button>
+                  </li>
+                </ul>
+              </form>
+            </div>
           </div>
-          <div className="contact-form">
-            <form ref={form} onSubmit={sendEmail}>
-              <ul>
-                <li className="half">
-                  <input type="text" required placeholder="Name" />
-                </li>
-                <li className="half">
-                  <input type="email" required placeholder="Email" />
-                </li>
-                <li>
-                  <input type="text" required placeholder="Subject" />
-                </li>
-                <li>
-                  <textarea placeholder="Message" required></textarea>
-                </li>
-                <li>
-                  <button type="submit" className="flat-btn">
-                    SEND
-                  </button>
-                </li>
-              </ul>
-            </form>
-          </div>
+        </div>
+        <div className="map-info">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="map"
+            style={{ filter: 'opacity(0.7)' }}
+            src="https://www.google.com/maps/embed/v1/place?q=Toamasina,+Madagascar&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
         </div>
       </div>
     </>
