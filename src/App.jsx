5989049d076abd6a814dc483ee27aee22798a5d1
Wenceslas Angelo <wenceslasangelo@gmail.com>
2023-03-10 13:50:35 +0300

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import './App.scss';
 
 import SideBar from './components/SideBar';
 import Home from './pages/Home';
-import About from './pages/About';
-import Work from './pages/Work';
+// import About from './pages/About';
+// import Work from './pages/Work';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-[#181818]">
       <SideBar />
       <div className="page-container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Work" element={<Work />} />
+          {/* <Route path="/About" element={<About />} />
+          <Route path="/Work" element={<Work />} />*/}
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Error />} />
         </Routes>
@@ -28,15 +28,3 @@ function App() {
 }
 
 export default App;
-{
-  /* <div className="map-info">
-        <iframe
-          width="100%"
-          height="100%"
-          title="map"
-          className="map"
-          style={{ filter: 'opacity(0.7)' }}
-          src="https://www.google.com/maps/embed/v1/place?q=Toamasina,+Madagascar&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-        />
-      </div> */
-}
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -7,39 +7,47 @@ function Contact() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1 className="">Contact Me</h1>
+      <h1 className="font-bold text-white text-6xl text-center">Contact</h1>
 
-      <div>
+      <div className="grid md:flex justify-center items-center mt-10 md:my-20">
         <img
           src={messagerie}
           title="New message"
           alt="illustration messagerie"
+          className="mx-auto w-2/3 md:w-1/3"
         />
 
-        <form>
-          <div>
+        <form className="md:w-[60%] w-full my-3" onSubmit={(e) => sendEmail(e)}>
+          <div className="w-full">
             <input
               type="text"
               placeholder="Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
+              className="bg-transparent border md:w-[45%] w-full text-white border-gray-400 outline-none m-2 rounded-sm p-1 text-[20px] focus:border-indigo-600"
             />
             <input
               type="text"
               placeholder="Subject"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
+              className="bg-transparent border md:w-[45%] w-full text-white border-gray-400 outline-none m-2 rounded-sm p-1 text-[20px] focus:border-indigo-600"
             />
           </div>
 
-          <div>
+          <div className="w-full">
             <input
               type="email"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="bg-transparent border md:w-[92%] w-full text-white border-gray-400 outline-none m-2 rounded-sm p-1 text-[20px] focus:border-indigo-600"
             />
           </div>
 
@@ -48,13 +56,28 @@ function Contact() {
               placeholder="Message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
+              className="bg-transparent border  h-[100px] md:w-[92%] w-full text-white border-gray-400 outline-none m-2 rounded-sm p-1 text-[20px] focus:border-indigo-600"
             ></textarea>
           </div>
 
-          <button disabled={!name || !subject || !email || !message}>
+          <button
+            disabled={!name || !subject || !email || !message}
+            className="text-2xl text-white py-1 cursor-pointer disabled:cursor-not-allowed m-2 md:w-[92%] w-full disabled:bg-gray-500 bg-indigo-600"
+          >
             Send
           </button>
         </form>
+      </div>
+
+      <div className="w-full h-[500px]">
+        <iframe
+          width="100%"
+          height="100%"
+          title="map"
+          className="map"
+          style={{ filter: 'opacity(0.7)' }}
+          src="https://www.google.com/maps/embed/v1/place?q=Toamasina,+Madagascar&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+        />
       </div>
     </div>
   );
